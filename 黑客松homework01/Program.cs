e3a0b248ab896606c0aff68532dd1c582067aa36
Jeff <jeff129939022@gmail.com>
2025-10-20 10:11:50 +0800

    加入專案檔案。

---
+++ 黑客松homework01/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace 黑客松homework01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = Console.ReadLine();
+            int nums = int.Parse(num);
+            for(int i = 1; i <= nums; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("dann");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("School");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Build");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+
+        }
+    }
+}
