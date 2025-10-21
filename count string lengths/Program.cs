b362127e286bdf2d0984e21ee0e9f60d73b063de
Jeff <jeff129939022@gmail.com>
2025-10-21 10:48:51 +0800

    推送計算字串

---
+++ count string lengths/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace count_string_lengths
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請輸入一個有意義的英文字串:");
+            string input = Console.ReadLine();
+            string[] item = input.Split(' ');
+            var list = item.ToList();
+
+            var count = list.Select(x => x.ToLower()).GroupBy(x => x).Select(x => new
+            {
+                Word = x.Key,
+                Count = x.Count(),
+            });
+            foreach(var word in count)
+            {
+                Console.WriteLine($"{word.Word} : {word.Count}");
+            }
+        }
+    }
+}
